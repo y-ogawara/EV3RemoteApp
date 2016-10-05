9b0a2287194fa5a2dd07f609af7d1f18fd6fc59b
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-10-05 20:29:16 +0900

    test

---
+++ app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
@@ -0,0 +1,13 @@
+package org.t_robop.y_ogawara.ev3remoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RunListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_list);
+    }
+}
