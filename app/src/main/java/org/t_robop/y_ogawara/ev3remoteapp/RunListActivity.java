9b713a5cbfd392a6d45966a92bbf3a93097e3e7f
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-10-05 21:53:15 +0900

    listにText追加されてくのと要素クリック時の処理を書きました

--- app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
@@ -3,9 +3,11 @@ package org.t_robop.y_ogawara.ev3remoteapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class RunListActivity extends AppCompatActivity {
 
@@ -24,9 +26,25 @@ public class RunListActivity extends AppCompatActivity {
 
         //関連付け
         listRun=(ListView)findViewById(R.id.list_run);
-
         //アダプターの初期化
-        //adapterRun=new ArrayAdapter<String>(this, R.layout.rowdata, data);
+        adapterRun=new ArrayAdapter<>(this, R.layout.run_list_item);
+
+        // アイテムクリック時ののイベントを追加
+        listRun.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int pos, long id) {
+
+                // 選択アイテムを取得
+                ListView listView = (ListView)parent;
+                String item = (String)listView.getItemAtPosition(pos);
+
+                Toast.makeText(RunListActivity.this, item, Toast.LENGTH_SHORT).show();
+
+
+                //ここで編集用ダイアログ出す
+
+            }
+        });
     }
 
     //前進ボタン押したー
@@ -40,8 +58,9 @@ public class RunListActivity extends AppCompatActivity {
     //アダプター追加処理
     public void addAdapter(){
 
+        //アダプターに追加
         adapterRun.add("やったぜ");
-
+        //アダプターセット
         listRun.setAdapter(adapterRun);
 
     }
