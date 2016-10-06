e5e017f49e868266bba658c61c6610db6315a343
KenFujita <kentyan199@yahoo.co.jp>
2016-10-06 19:04:14 +0900

    ボタン押し→numberpicker 値の受け渡し完了

--- app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/ButtonActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/ButtonActivity.java
@@ -5,31 +5,78 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class ButtonActivity extends AppCompatActivity {
     Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
     TextView txt1;
+    ListView lst1;
+    ArrayAdapter<String> adapter;
+    String str2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
 
-        btn1 = (Button)findViewById(R.id.button);
+        btn1 = (Button)findViewById(R.id.button);                                       //うしろ
+        btn2 = (Button)findViewById(R.id.button5);                                      //ひだり
+        btn3 = (Button)findViewById(R.id.button6);                                      //みぎ
+        btn4 = (Button)findViewById(R.id.button7);                                      //まえ
         txt1 = (TextView)findViewById(R.id.textView);
+        lst1 = (ListView)findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        lst1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                onSend(view);
+            }
+        });
     }
 
     public void onSend(View v){
-        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+        //**********************************方向判定*****************************//
+        switch(v.getId()){
+            case R.id.button:
+                str2 = "後";
+                break;
+            case R.id.button5:
+                str2 = "左";
+                break;
+            case R.id.button6:
+                str2 = "右";
+                break;
+            case R.id.button7:
+                str2 = "前";
+                break;
+            default:
+                break;
+        }
+        //********************************************************************//
+        AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);                       //ダイアログの生成
         alertDlg.setTitle("ダイアログタイトル");
-        alertDlg.setMessage("押せ");
-        alertDlg.setPositiveButton(
+        final NumberPicker np1 = new NumberPicker(ButtonActivity.this);                     //ダイアログ中の数字ロール生成
+        np1.setMaxValue(10);                                                                //上限設定
+        np1.setMinValue(1);                                                                 //下限設定
+        alertDlg.setView(np1);
+        alertDlg.setPositiveButton(                                                         //ボタン押された処理
                 "OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // OK ボタンクリック処理
-                        txt1.setText("あああ");
+                       /* String str1 = String.valueOf(np1.getValue());
+                        adapter.add(str2+" "+str1);
+                        lst1.setAdapter(adapter);//*/
                     }
                 });
         alertDlg.setNegativeButton(
@@ -37,7 +84,6 @@ public class ButtonActivity extends AppCompatActivity {
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // Cancel ボタンクリック処理
-                        txt1.setText("ぽやしみ。");
                     }
                 });
 
