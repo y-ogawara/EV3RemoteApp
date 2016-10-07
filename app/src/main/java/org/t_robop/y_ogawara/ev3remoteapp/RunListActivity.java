f8493ef03b96822ff2befaf3f7f4b56059fd6ade
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-10-07 21:10:28 +0900

    f〇ck!

--- app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
@@ -1,12 +1,17 @@
 package org.t_robop.y_ogawara.ev3remoteapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RunListActivity extends AppCompatActivity {
@@ -19,6 +24,14 @@ public class RunListActivity extends AppCompatActivity {
     //テスト用前進ボタン
     Button btnMae;
 
+    AlertDialog.Builder alertDlg;
+
+    View inputView;
+
+    EditText dialogEdit;
+
+    TextView dialogText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +41,14 @@ public class RunListActivity extends AppCompatActivity {
         listRun=(ListView)findViewById(R.id.list_run);
         //アダプターの初期化
         adapterRun=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        //レイアウトの呼び出し
+        LayoutInflater factory = LayoutInflater.from(this);
+        inputView = factory.inflate(R.layout.dialog_edit, null);
+
+        //ダイアログ内の関連付け
+        dialogEdit =(EditText)inputView.findViewById(R.id.dialog_edit);
+        dialogText=(TextView)inputView.findViewById(R.id.dialog_text);
 
         // アイテムクリック時ののイベントを追加
         listRun.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -50,18 +71,52 @@ public class RunListActivity extends AppCompatActivity {
     //前進ボタン押したー
     public void zensin(View v){
 
-        //アダプター追加からのセット
-        addAdapter();
+        setDialog("前進");
 
     }
 
     //アダプター追加処理
-    public void addAdapter(){
+    public void addAdapter(String action,int second){
 
         //アダプターに追加
-        adapterRun.add("やったぜ");
+        adapterRun.add(action+"【"+String.valueOf(second)+"秒】");
         //アダプターセット
         listRun.setAdapter(adapterRun);
+
+    }
+
+    //ダイアログ生成メソッド
+    public void setDialog(String title){
+
+        dialogText.setText(title);
+
+        if(alertDlg==null) {
+
+            alertDlg = new AlertDialog.Builder(this)
+            .setView(inputView)
+            .setPositiveButton(
+                    "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // OK ボタンクリック処理
+
+                            //入力されたText取得
+                            int second=Integer.parseInt(dialogEdit.getText().toString());
+
+                            //アダプター追加からのセット
+                            addAdapter(dialogText.getText().toString(),second);
+                        }
+                    })
+            .setNegativeButton(
+                    "Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Cancel ボタンクリック処理
+                        }
+                    });
+            alertDlg.create();
+        }
+        alertDlg.show();
 
     }
 }
