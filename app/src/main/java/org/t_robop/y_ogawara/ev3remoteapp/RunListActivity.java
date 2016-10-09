ccd71c7c165b587a39a53963017e2d90e0792b57
taiga <6bji2119@mail.u-tokai.ac.jp>
2016-10-09 23:21:43 +0900

    リストの追加と編集おわった

--- app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
+++ app/src/main/java/org/t_robop/y_ogawara/ev3remoteapp/RunListActivity.java
@@ -8,28 +8,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class RunListActivity extends AppCompatActivity {
 
+    //ArrayListに追加➝ArrayListを元にしたArrayAdapterを作成➝ListにArrayAdapterを反映
+
+    /**リスト関連**/
     //実行する処理のリスト
     ListView listRun;
     //実行する処理用のアダプター
     ArrayAdapter<String> adapterRun;
+    //リスト編集やるためのArrayList
+    ArrayList arrayListRun;
+    //リストのどの要素をクリックしたかを知るためのグローバル変数
+    int touchPos;
 
-    //テスト用前進ボタン
-    Button btnMae;
-
+    /**ダイアログ設定関連**/
+    //ダイアログ
     AlertDialog alertDlg;
+    //ダイアログ内の処理分け用グローバル変数(0:追加処理,1:編集処理)
+    int NUM;
 
+    /**ダイアログレイアウト関連**/
+    //ダイアログのレイアウトを取得するView
     View inputView;
-
+    //ダイアログ内のEditText(こいつを弄ることで何処からでもダイアログ内のEditTextを弄れるゾ！)
     EditText dialogEdit;
-
+    //ダイアログ内のTextView(こいつを弄ることで何処からでもダイアログ内のTextViewを弄れるゾ！)
     TextView dialogText;
 
     @Override
@@ -37,85 +47,83 @@ public class RunListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run_list);
 
-        //関連付け
+        //リストの関連付け
         listRun=(ListView)findViewById(R.id.list_run);
-        //アダプターの初期化
-        adapterRun=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
 
-        //レイアウトの呼び出し
+        /**list関連の初期設定**/
+        //ArrayListの初期化
+        arrayListRun=new ArrayList<String>();
+        //アダプターの初期化
+        adapterRun=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayListRun);
+
+        /**ダイアログレイアウトの呼び出し**/
+        //ダイアログレイアウトの読み込み
         LayoutInflater factory = LayoutInflater.from(this);
         inputView = factory.inflate(R.layout.dialog_edit, null);
-
         //ダイアログ内の関連付け
         dialogEdit =(EditText)inputView.findViewById(R.id.dialog_edit);
         dialogText=(TextView)inputView.findViewById(R.id.dialog_text);
 
-        // アイテムクリック時ののイベントを追加
-        listRun.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent,
-                                    View view, int pos, long id) {
+        //リストをクリックした時のイベント追加
+        setListClick();
 
-                // 選択アイテムを取得
-                ListView listView = (ListView)parent;
-                String item = (String)listView.getItemAtPosition(pos);
-
-                Toast.makeText(RunListActivity.this, item, Toast.LENGTH_SHORT).show();
-
-                //listから数値のみ取得してダイアログ内のedittextに貼る
-                int ret = Integer.parseInt(item.substring(4-1).replaceAll("[^0-9]",""));
-                dialogEdit.setText(String.valueOf(ret));
-
-                //ここで編集用ダイアログ出す
-                showDialog(item.substring(0,2));//現在のリストの場所+1,先頭二文字
-
-            }
-        });
-
-        //初回ダイアログセット
+        //ダイアログのセット
         setDialog();
-
     }
 
-    //前進ボタン押したー
+    //前進ボタン押した時ッ(ボタン系はこの中身の通りにしてやれば動くはず)
     public void zensin(View v){
-
         //ダイアログ内のedittextクリーン
         resetEdit(dialogEdit,dialogText);
-
         //ダイアログ表示
-        showDialog("前進");
+        showDialog("前進",0);
     }
 
-    //アダプター追加処理(入力した文字列と入力された秒数を一緒に追加すっぞ)
-    public void addAdapter(String action,int second){
-
-        //アダプターに追加
-        adapterRun.add(action+"【"+String.valueOf(second)+"秒】");
+    //List更新処理(ArrayListの情報をadapter&listに反映すっぞ)
+    public void setList(){
+        //アダプターの更新
+        adapterRun=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayListRun);
         //アダプターセット
         listRun.setAdapter(adapterRun);
-
     }
 
     //ダイアログ生成メソッド(起動時に呼んであげて)
     public void setDialog(){
 
+        //ダイアログが精製されてない時(つまり初回のみ)
         if(alertDlg==null) {
 
-            alertDlg = new AlertDialog.Builder(RunListActivity.this)
+            //新しくダイアログ作ったるで！
+            alertDlg= new AlertDialog.Builder(RunListActivity.this)
+            //ダイアログレイアウトに使うViewを指定
             .setView(inputView)
+            //ok押した時のイベントセット
             .setPositiveButton(
                     "OK",
                     new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             // OK ボタンクリック処理
 
-                            //入力されたText取得
-                            int second=Integer.parseInt(dialogEdit.getText().toString());
-
-                            //アダプター追加からのセット
-                            addAdapter(dialogText.getText().toString(),second);
+                            //入力された文字列の文字数が0でない時(何か入力されてる時)
+                            if(dialogEdit.getText().toString().length()!=0) {
+                                //入力されたText取得
+                                int second = Integer.parseInt(dialogEdit.getText().toString());
+                                //新規作成時
+                                if (NUM == 0) {
+                                    //ArrayListに追加
+                                    arrayListRun.add(dialogText.getText().toString() + "【" + String.valueOf(second) + "秒】");
+                                }
+                                //編集時
+                                else if (NUM == 1) {
+                                    //選択された要素を編集
+                                    arrayListRun.set(touchPos, dialogText.getText().toString() + "【" + String.valueOf(second) + "秒】");
+                                }
+                                //List更新
+                                setList();
+                            }
                         }
                     })
+            //Cancel押した時
             .setNegativeButton(
                     "Cancel",
                     new DialogInterface.OnClickListener() {
@@ -127,21 +135,50 @@ public class RunListActivity extends AppCompatActivity {
         }
     }
 
-    public void showDialog(String title){
+    //リストの要素クリックした時
+    public void setListClick(){
 
-        dialogText.setText(title);
+        //アイテムクリック時ののイベントを追加
+        listRun.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View view, int pos, long id) {
 
-        alertDlg.show();
+                //今クリックした要素のポジションを取得
+                touchPos=pos;
 
+                //選択アイテムを取得
+                ListView listView = (ListView)parent;
+                String item = (String)listView.getItemAtPosition(pos);
+
+                //listから数値のみ取得
+                int ret = Integer.parseInt(item.substring(4-1).replaceAll("[^0-9]",""));
+                //数値がある時(安全設計)
+                if(String.valueOf(ret)!="") {
+                    //ダイアログ内のedittextに貼る
+                    dialogEdit.setText(String.valueOf(ret));
+                }
+
+                //ここで編集用ダイアログ出す
+                showDialog(item.substring(0,2),1);//先頭二文字をタイトルに
+            }
+        });
     }
 
-    /**新規追加用に一度edittextをクリーンできるメソッド**/
-    public void resetEdit(EditText edit,TextView view){
+    //タイトルと設定した数字でダイアログが表示されます
+    public void showDialog(String title,int another){
+        //追加処理or編集処理
+        NUM=another;
+        //指定されたタイトルをダイアログ内のTextViewにセット
+        dialogText.setText(title);
+        //ダイアログ展開
+        alertDlg.show();
+    }
 
+    //新規追加用に一度edittextをクリーンできるメソッド
+    public void resetEdit(EditText edit,TextView view){
         //edittextの内容を削除
         edit.getEditableText().clear();
         //textView(edittext以外のView)にフォーカスを移す
         view.requestFocus();
-
     }
 }
